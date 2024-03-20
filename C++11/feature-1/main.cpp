a226ec9ac56f68b4301dd39307567495a4380732
huaqfantasy <hjf734628@gmail.com>
2024-03-20 18:04:35 +0800

    add source code

--- C++11/feature-1/main.cpp
+++ C++11/feature-1/main.cpp
@@ -1,6 +1,22 @@
 ﻿#include <iostream>
 
 int main(){
+    auto i=10;
+    std::cout<<typeid(i).name()<<"\n";
+    auto j=2l;
+    std::cout<<typeid(j).name()<<"\n";
+    auto k=3lu;
+    std::cout<<typeid(k).name()<<"\n";
+    auto l=3.1415;
+    std::cout<<typeid(l).name()<<"\n";
 
+    decltype(i) res = i * 2;
+    //static_assert(std::is_same_v<decltype(i), decltype(res)>); c++17 feature
+    //static_assert(std::is_same_v<decltype(i), decltype(j)>); error
+    std::cout<<typeid(res).name()<<"\n";
+   
+    
+
+    
     return 0;
 }
---
+++ C++11/feature-2/main.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+
+class Sample
+{
+private:
+    /* data */
+    int s;
+public:
+    Sample(/* args */);
+    ~Sample();
+    Sample(Sample&&) = delete;
+};
+
+
+int main(){
+    Sample s1; //success default
+    //Sample s2(s1); error delete
+    
+    return 0;
+}
+
+Sample::Sample(/* args */)
+{
+    std::cout<<"default constructor"<<std::endl;
+}
+
+Sample::~Sample()
+{
+    std::cout<<"default destructor"<<std::endl;
+}
