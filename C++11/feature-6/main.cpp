d921b10c1c11719002319901104e106ddac94ce7
HMS_Yukikaze <hjf734628@gmail.com>
2025-10-21 08:44:00 +0800

    uincode sport,temp

---
+++ C++11/feature-6/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+
+int main(){
+    auto str="哈哈哈";
+    auto str1=u8"哈哈哈";
+    std::cout<<"str: "<<str<<std::endl;
+    std::cout<<"str1: "<<str1<<std::endl;
+}
